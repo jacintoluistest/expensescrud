ec523cb624b35243e9086ca799b30c3960094e0f
jacintoluistest <luisj81@gmail.com>
2024-07-25 21:57:46 -0600

    setting files

---
+++ src/main/java/com/probatio/expensescrud/ExpensescrudApplication.java
@@ -0,0 +1,13 @@
+package com.probatio.expensescrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensescrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensescrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/probatio/expensescrud/ExpensescrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.probatio.expensescrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensescrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
